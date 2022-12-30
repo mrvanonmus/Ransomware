47d8b61f26db30d3c4eb41ee14c4bb29f6890ea6
alhamwi <64444222+mrvanonmus@users.noreply.github.com>
2022-12-30 20:18:18 +0300

    Update dec.sh

--- modules/dec.sh
+++ modules/dec.sh
@@ -6,7 +6,7 @@ from Crypto.Util import Counter
 import time
 import marshal
 time.clock = time.time
-dir_f_list ="alhamwi"
+dir_f_list = str
 files=dir_f_list('$3')
 kay='$1'" > $2
 cat modules/decryption >> $2
